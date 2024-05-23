6ef1b0e25a94c45e3215b351745080d9c8b08907
Andranik <andranik.boyajyan@bk.ru>
2024-05-23 13:40:10 +0400

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import FooterContainer from "./containers/FooterContainer";
 import HeaderContainer from "./containers/HeaderContainer";
+import './App.css'
 
 function App() {
   return (
-    <>
+    <div className="font-face-ub">
       <HeaderContainer />
       <FooterContainer />
-    </>
+    </div>
   );
 }
 
 export default App;
+
