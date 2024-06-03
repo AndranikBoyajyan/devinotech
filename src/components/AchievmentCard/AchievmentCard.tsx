ea0bc9b0a03eca08ba6f19fa24336e5d8bb60675
Andranik <andranik.boyajyan@bk.ru>
2024-06-03 14:12:15 +0400

    sdsadf

--- src/components/AchievmentCard/AchievmentCard.tsx
+++ src/components/AchievmentCard/AchievmentCard.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 import CupIcon from "../../../public/images/Achievments/CupIcon";
 import DesignIcon from "../../../public/images/Achievments/DesignIcon";
 import TechIcon from "../../../public/images/Achievments/TechIcon";
-import styles from './AchievmentCard.module.css'
+import styles from "./AchievmentCard.module.css";
 
 interface IProps {
   title: "Brand Idenity" | "Technology" | "UX/UI Design";
@@ -13,23 +13,7 @@ const AchievmentCard: FC<IProps> = ({ title }) => {
     "Bringing the history of your brand to the forefront gives an emotional dimension to your visual identity, which is essential today more than ever in today's digital landscape. ";
 
   return (
-    <div
-<<<<<<< HEAD
-      style={{
-        borderRadius: "28px",
-        backgroundColor: "#FFFFFF",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "baseline",
-        width: "20vw",
-        justifyContent: "center",
-        padding: "0px 15px",
-        gap: "3vh",
-      }}
-=======
-      className={styles.container}
->>>>>>> ca8e9f670b078e1cb3032e466c858e554074f197
-    >
+    <div className={styles.container}>
       {title === "Brand Idenity" ? (
         <CupIcon />
       ) : title === "Technology" ? (
@@ -38,11 +22,8 @@ const AchievmentCard: FC<IProps> = ({ title }) => {
         <DesignIcon />
       )}
       <div>{title}</div>
-<<<<<<< HEAD
       <div style={{ color: "#667085" }}>{description}</div>
-=======
       <div className={styles.divColor}>{description}</div>
->>>>>>> ca8e9f670b078e1cb3032e466c858e554074f197
     </div>
   );
 };
